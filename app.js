288b8bf6a59bdc8128c670191e01b3f7c6c229ac
Abhishek goyal <abhishekgoyal1311@gmail.com>
2024-12-16 12:21:37 +0530

    Added favicon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
-
+app.use(express.static('public'));
 
 // Connecting to the mongoDB.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
